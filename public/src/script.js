dffed9da46b98610a05959d3b3544f2d898947d8
IJlnflhbrzDev <IJlnflhbrzDev@gmail.com>
2020-12-17 21:17:28 +0700

    + desaign Footer & bg-gradients & time dll

--- public/src/script.js
+++ public/src/script.js
@@ -3,3 +3,11 @@ AOS.init();
     $(window).load(function () {
       $(".loader").fadeOut("slow");
     });
+
+    window.setTimeout("waktu()" , 1000)
+
+    function waktu() {
+      var waktu = new Date();
+      setTimeout("waktu()", 1000);
+      document.querySelector("footer .card-header").innerHTML = waktu.toUTCString();
+    }
