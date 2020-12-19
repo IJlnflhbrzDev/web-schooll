00ffb8ea9384f1032ddb5117dc6c819779d2920b
IJlnflhbrzDev <IJlnflhbrzDev@gmail.com>
2020-12-19 20:24:37 +0700

    + animation scrolling change => navbar

--- public/src/script.js
+++ public/src/script.js
@@ -11,3 +11,9 @@ AOS.init();
       setTimeout("waktu()", 1000);
       document.querySelector("footer .card-header").innerHTML = waktu.toUTCString();
     }
+
+    window.addEventListener('scroll',function () {
+      let _CompNavbars = document.querySelector('#_CompNavbars nav');
+      let window_position = window.scrollY > 0 ;
+      _CompNavbars.classList.toggle('animated_scrolling_change' ,window_position );
+    });
